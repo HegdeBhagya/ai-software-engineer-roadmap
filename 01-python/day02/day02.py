d0b24b4bf7d79f3494bd0313d308d14f1fdc9bcb
HegdeBhagya <bhagyalaxmimhegde@gmail.com>
2026-08-07 23:13:58 +0530

    Day 2: Python variables and data types

---
+++ 01-python/day02/day02.py
@@ -0,0 +1,87 @@
+﻿#Datatypes
+'''
+Name="Bhagya"
+age=25
+
+print(Name)
+print(age)
+
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+age1=int(input("Age: "))
+
+print(name)
+print(age)
+print(age1)
+
+print(age + 1)   # throws error because age is a string , we need to convert it into integer before adding 1 to it
+print(age1 + 1)
+'''
+# Output
+'''
+Bhagya
+25
+Enter your name: Ramu
+Enter your age: 34
+Age: 35
+Ramu
+34
+35
+Traceback (most recent call last):
+  File "D:\AI-Roadmap-2026\AI-Learning\ai-software-engineer-roadmap\01-python\day02\day02.py", line 16, in <module>
+    print(age + 1)
+          ~~~~^~~
+TypeError: can only concatenate str (not "int") to str
+'''
+
+
+
+#Code for AI Engineer Learning Tracker
+
+print("=" * 50)
+print("AI Engineer Learning Tracker")
+print("=" * 50)
+
+name = input("Enter your name: ")
+role = input("Target role: ")
+study_hours = float(input("Daily study hours: "))
+study_hours1 = input("Daily study hours: ")
+python_level = input("Python Level: ")
+
+weekly_hours = study_hours * 7
+
+print("\n===== SUMMARY =====")
+print(f"Name          : {name}")
+print(f"Target Role   : {role}")
+print(f"Python Level  : {python_level}")
+print(f"Daily Hours   : {study_hours}")
+print(f"Weekly Hours  : {weekly_hours}")
+
+print("\nKeep showing up every day! 🚀")
+print(type(name))
+print(type(study_hours))
+print(type(study_hours1))
+#Output
+'''
+==================================================
+AI Engineer Learning Tracker
+==================================================
+Enter your name: Bhagya
+Target role: AI Engineer
+Daily study hours: 3
+Daily study hours: 3
+Python Level: Intemediate
+
+===== SUMMARY =====
+Name          : Bhagya
+Target Role   : AI Engineer
+Python Level  : Intemediate
+Daily Hours   : 3.0
+Weekly Hours  : 21.0
+
+Keep showing up every day! 🚀
+<class 'str'>
+<class 'float'>
+<class 'str'>
+'''
+
